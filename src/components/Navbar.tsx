fbf5453b6cb3300a41a2fbbe353bc2a8e7b43c02
Alam Barkani <alambarkani@gmail.com>
2024-03-26 21:05:03 +0700

    Implement Darkmode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,17 @@
 
-import { Button, Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
+import { Button, DarkThemeToggle, Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
+// import DarkmodeSwitch from "./DarkModeSwitcher";
 
 export default function TopNavbar() {
   return (
     <Navbar fluid rounded>
       <NavbarBrand href="https://flowbite-react.com">
-        <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+        <img src="/vite.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
       </NavbarBrand>
       <div className="flex md:order-2">
         <Button>Get started</Button>
+        <DarkThemeToggle />
         <NavbarToggle />
       </div>
       <NavbarCollapse>
