d5e4fbbb6fb2c486f143c66c722f9ad995cc6ec4
Alam Barkani <alambarkani@gmail.com>
2024-03-26 20:54:32 +0700

    Add Home Page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import About from "../components/About";
+import Contact from "../components/Contact";
+import HeroSection from "../components/Hero";
+import ProjectSection from "../components/Project";
+
+export default function Home(){
+
+    return (
+        <>
+            <HeroSection />
+            <ProjectSection />
+            <About />
+            <Contact />
+        </>
+    )
+}
